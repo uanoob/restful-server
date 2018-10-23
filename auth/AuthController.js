a9e926c6b6706bdd2b57f4b72b83f0308c60ebbc
uanoob <usnet@ukr.net>
2018-10-23 15:30:40 +0300

    Add logout route

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -44,4 +44,21 @@ router.get('/me', function(req, res) {
   });
 });
 
+router.post('/login', function(req, res) {
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) return res.status(500).send('Error on the server.');
+    if (!user) return res.status(404).send('No user found.');
+    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+    var token = jwt.sign({ id: user._id }, config.secret, {
+      expiresIn: 86400 // expires in 24 hours
+    });
+    res.status(200).send({ auth: true, token: token });
+  });
+});
+
+router.get('/logout', function(req, res) {
+  res.status(200).send({ auth: false, token: null });
+});
+
 module.exports = router;
