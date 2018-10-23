e741799dc49c9bed1dd560688b30f3c514846cc6
uanoob <usnet@ukr.net>
2018-10-23 15:21:11 +0300

    Create AuthController

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 
+var AuthController = require('./auth/AuthController');
+app.use('/api/auth', AuthController);
+
 var UserController = require('./user/UserController');
 app.use('/users', UserController);
 
