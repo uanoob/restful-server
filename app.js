e9b8490832bee21246db18ed8339db90f6784dcd
uanoob <usnet@ukr.net>
2018-10-23 16:06:17 +0300

    Add cors

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 var AuthController = require('./auth/AuthController');
 app.use('/api/auth', AuthController);
